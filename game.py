54c87a29df3a820819c180b855ec07f3e06c1869
Pitchayapon Thawornwong <65160182@go.buu.ac.th>
2026-04-29 18:04:24 +0700

    start

---
+++ game.py
@@ -0,0 +1,6 @@
+m = 1
+i = 1
+while m < 50:
+    print(m)
+    m = m + i
+    i = i + 1
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,10 +6,79 @@
       </q-card-section>
     </q-card>
   </div>
+  <div class="q-pa-md">
+    <q-markup-table flat bordered>
+      <thead class="bg-teal">
+        <tr>
+          <th colspan="5">
+            <div class="row no-wrap items-center">
+              <div class="text-h4 q-ml-md text-white">Book Store</div>
+
+              <div class="q-pa-md">
+                <q-btn-dropdown color="primary" label="Dropdown Button">
+                  <q-list>
+                    <q-item clickable v-close-popup @click="onItemClick">
+                      <q-item-section>
+                        <q-item-label>Add Book</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-btn-dropdown>
+              </div>
+            </div>
+          </th>
+        </tr>
+        <tr>
+          <th class="text-left">Dessert (100g serving)</th>
+          <th class="text-right">Calories</th>
+          <th class="text-right">Fat (g)</th>
+          <th class="text-right">Carbs (g)</th>
+          <th class="text-right">Protein (g)</th>
+        </tr>
+      </thead>
+      <tbody :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'">
+        <tr>
+          <td class="text-left">Frozen Yogurt</td>
+          <td class="text-right">159</td>
+          <td class="text-right">6</td>
+          <td class="text-right">24</td>
+          <td class="text-right">4</td>
+        </tr>
+        <tr>
+          <td class="text-left">Ice cream sandwich</td>
+          <td class="text-right">237</td>
+          <td class="text-right">9</td>
+          <td class="text-right">37</td>
+          <td class="text-right">4.3</td>
+        </tr>
+        <tr>
+          <td class="text-left">Eclair</td>
+          <td class="text-right">262</td>
+          <td class="text-right">16</td>
+          <td class="text-right">23</td>
+          <td class="text-right">6</td>
+        </tr>
+        <tr>
+          <td class="text-left">Cupcake</td>
+          <td class="text-right">305</td>
+          <td class="text-right">3.7</td>
+          <td class="text-right">67</td>
+          <td class="text-right">4.3</td>
+        </tr>
+        <tr>
+          <td class="text-left">Gingerbread</td>
+          <td class="text-right">356</td>
+          <td class="text-right">16</td>
+          <td class="text-right">49</td>
+          <td class="text-right">3.9</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </div>
 </template>
 
 <script setup>
-const lorem = 'Welcome to the Coffee Shop! This is a simple Quasar application that demonstrates how to create a card component using Vue.js and Quasar Framework. You can customize this card with your own content and styles. Enjoy your coffee!'
+const lorem = 'ทดสอบการแสดงผลค้าบ'
 </script>
 
 <style lang="sass" scoped>
